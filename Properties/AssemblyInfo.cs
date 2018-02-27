fc9555ecb0acef57716135a151bb51a889f536d4
starlying <corp@siteserver.cn>
2018-02-27 08:45:53 +0800

    assembly

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SS.Magazine")]
+[assembly: AssemblyDescription("SiteServer CMS Magazine plugin.")]
+[assembly: AssemblyCopyright("Copyright © SiteServer CMS 2018")]
+[assembly: Guid("ff6cf89a-4fb2-475f-91d5-68d46c6f7017")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
