282794228af2666b7ca1befa079bc7dfc9c99fa1
starlying <starlying@gmail.com>
2018-05-31 11:26:16 +0800

    plugin1.7

--- Parse/StlMagazineArticles.cs
+++ Parse/StlMagazineArticles.cs
@@ -47,7 +47,7 @@ namespace SS.Magazine.Parse
             var apiUrl = Main.Instance.PluginApi.GetPluginApiUrl(nameof(ApiArticles));
             var guid = "e" + Guid.NewGuid().ToString().Replace("-", string.Empty);
 
-            var parsedContent = Main.Instance.ParseApi.ParseInnerXml(context.StlInnerXml, context);
+            var parsedContent = Main.Instance.ParseApi.Parse(context.StlInnerHtml, context);
             if (!string.IsNullOrEmpty(parsedContent))
             {
                 parsedContent = $@"
